dfbc9472aeafaa9cc5ff93ffd6ba71eed3b46ec2
Idraezy <idraezynoks@gmail.com>
2025-11-29 00:48:41 +0100

    changed alot

--- components/DisplayCard.tsx
+++ components/DisplayCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 
 interface DisplayCardProp{
   loading?: boolean;
@@ -8,23 +8,19 @@ interface DisplayCardProp{
   descColor: string;
 }
 
-const DisplayCard = ({loading, title, description, isSpanTwo, descColor}: DisplayCardProp) => {
+const DisplayCard = ({ loading, title, description, isSpanTwo, descColor }: DisplayCardProp) => {
   return (
-    <div className={`border-border border bg-card rounded-xl flex flex-col uppercase items-center py-4 ${isSpanTwo ? "col-span-2" : ""}`}>
-      <h1 className='text-lg text-gray font-bold'>{title}</h1>
+    <div className={`border-border bg-card border rounded-xl flex flex-col uppercase items-center py-4 ${isSpanTwo ? "col-span-2" : ""}`}>
+      <h1 className="text-lg text-gray font-bold">{title}</h1>
       <div>
         {
-
-          loading? (
-            <div className={`animate-pulse text-xl font-bold ${descColor}`}>loading....</div>
+          loading ? (
+            <div className={`animate-pulse text-xl font-bold ${descColor}`}>loading...</div>
           ) : (
             <p className={`text-xl font-bold ${descColor}`}>{description}</p>
           )
-    
         }
       </div>
-    
-    DisplayCard
     </div>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,37 @@
+'use client';
 
+import { getWalletClient } from '@/lib/viem';
 import Button from './Button'
-
 import ConnectionStatus from './ConnectionStatus'
+import { useWalletContext } from '@/context/WalletProvider'
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+  const { walletAddress, isConnected, setWalletAddress, setIsConnected } = useWalletContext();
+  
+
+  const handleClick = async() => {
+    const walletClient = getWalletClient();
+    const [address] = await walletClient.requestAddresses();
+    setWalletAddress(address);
+    setIsConnected(true);
+  }
+
+  useEffect(() => {
+    console.log({walletAddress, isConnected});
+  }, [walletAddress, isConnected])
+ 
   return (
-    <header className='bg-card border-b border-border w-full flex-1'>
-        <div className='flex justify-end h-[60px] items-center pr-5'>
-          <div className='flex gap-[50px] items-center '>
-            <ConnectionStatus walletAddress='0x3a12F0b7C9E4cA8d4bA7e2F5f12D09D8J7893BNHJ78'/>
-            <Button text="click me" />
+    <header className=' bg-card border-b border-border w-full flex-1'>
+      <div className="flex justify-end h-[60px] items-center pr-5">
+          <div className='flex gap-[50px] items-center'>
+              {isConnected && walletAddress && <ConnectionStatus walletAddress={walletAddress} />}
+              <Button 
+                text={isConnected ? "Wallet Connected" : "Connect Wallet"} 
+                onClick={handleClick}
+              />
           </div>
-        </div>
+      </div>
     </header>
   )
 }
